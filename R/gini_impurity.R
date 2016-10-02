4c48c7e10850d38f5045a69453026fed857682c1
Ben Gorman <bgorman519@gmail.com>
2016-10-02 16:21:51 -0500

    Updated gini impurity

--- R/gini_impurity.R
+++ R/gini_impurity.R
@@ -1,10 +1,26 @@
+#' @title
 #' Gini Impurity
+#'
+#' @description
+#' Calculates the Gini Impurity of a set. Gini Impurity is a measure of how often a randomly chosen element from the set would be
+#' incorrectly labeled if it was randomly labeled according to the distribution of labels in the set. Values may be provided as a
+#' vector or via a pair of parameters - a data.table object and the name of the column to bin.
+#'
+#' @details
+#' Returns a real value in the range [0, 1]
+#'
+#' @param vals A vector of values. Values can be given as raw instances like c("red", "red", "blue", "green") or as a named vector
+#' of class frequencies like c(red=2, blue=1, green=1)
+#'
+#' @examples
+#' gini_impurity(c("red", "red", "blue", "green"))
+#' gini_impurity(c(red=2, blue=1, green=1))
 #'
 #' @export
 
 gini_impurity <- function(vals){
-  # Returns the gini impurity of a set of categorical values
-  # vals can either be the raw category instances (vals=c("red", "red", "blue", "green")) or named category frequencies (vals=c(red=2, blue=1, green=1))
+  # Returns the gini impurity of a set of values
+  # vals can either be raw category instances (e.g. c("red", "red", "blue", "green")) or named category frequencies (e.g. c(red=2, blue=1, green=1))
   # Gini Impurity is the probability a value is incorrectly labeled when labeled according to the distribution of classes in the set
 
   if(is(vals, "numeric")) counts <- vals else counts <- table(vals)
