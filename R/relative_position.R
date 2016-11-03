c6a97e8f24db6561ec6e58663a5b3ff5c55b907b
Ben Gorman <bgorman519@gmail.com>
2016-11-03 13:56:52 -0500

    Updated doc for relative_position()

--- R/relative_position.R
+++ R/relative_position.R
@@ -1,10 +1,25 @@
-#' Get relative positions
+#' @title
+#' Relative Position
+#' 
+#' @description
+#' Scale a vector of values to the range [0, 1] based on rank/position
+#' 
+#' @details
+#' Values are ranked and then scaled to the range [0, 1]. Ties result in the same relative position 
+#' (e.g. relative_position(c(1, 2, 2, 3)) results in c(0.0 0.5 0.5 1.0)). NAs remain as NAs.
+#' 
+#' @param vals vector of values
+#'
+#' @examples
+#' relative_position(1:10)
+#' relative_position(c(1, 2, 2, 3))
+#' relative_position(c(1, NA, 3, 4))
 #'
 #' @export
 #' @import data.table
 
 relative_position <- function(vals){
-  # Returns the ranked position of each value, scaled to (0, 1)
+  # Returns the ranked position of each value, scaled to [0, 1]
 
   if(length(vals) == 1){
     if(is.na(vals)) return(NA) else return(0.5)
