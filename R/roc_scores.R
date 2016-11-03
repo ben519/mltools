47a24b50a765b48b446b354888b10965e7babd1d
Ben Gorman <bgorman519@gmail.com>
2016-11-03 13:39:45 -0500

    Updated docs for roc_scores()

--- R/roc_scores.R
+++ R/roc_scores.R
@@ -1,22 +1,44 @@
-#' Get ROC scores
+#' @title
+#' ROC scores
+#' 
+#' @description
+#' This function provides a way to identify the worst predictions when measuring Area Under the ROC curve. Simply
+#' put, the 'worst' predictions are the ones with very low or high relative prediction scores (usually probabilities) 
+#' which relate to the positive and negative samples respectively.
+#' 
+#' @details
+#' How it works
+#' \itemize{
+#' \item{First the relative position (between 0 and 1) of each prediction is determined}
+#' \item{Next the mean of actuals is determined}
+#' \item{For samples whose position is on the correct side of the overall mean, 0 is given}
+#' \item{For samples whose position is on the wrong side of the overall mean, its distance from the mean is given}
+#' }
+#' 
+#' @param preds vector of predictions (need not be in range [0-1] - only order matters)
+#' @param actuals vector of actuals - either logical or vector of 1s and 0s
+#'
+#' @examples
+#' roc_scores(c(1,2,3,4), actuals=c(1,1,0,0))
+#' roc_scores(c(0.1, 0.2, 0.3, 0.4), actuals=c(TRUE, FALSE, TRUE, FALSE))
 #'
 #' @export
 #' @import data.table
 
 roc_scores <- function(preds, actuals){
   # Returns a score for each prediction (between 0 (good) and 1 (bad))
-
+  
   # How it works:
   # First the relative position (between 0 and 1) of each prediction is determined
   # Next the mean of actuals is determined
   # For samples whose position is on the correct side of the overall mean, 0 is given
   # For samples whose position is on the wrong side of the overall mean, its distance from the mean is given
-
+  
   overall <- mean(actuals)
   dt <- data.table(Pred=preds, Actual=actuals)
   dt[, ROCPosition := (frank(Pred) - 1) / (.N - 1)]
   dt[, Err := abs(ROCPosition - overall)]
   dt[(Actual==0 & ROCPosition <= overall) | (Actual==1 & ROCPosition >= overall), Err := 0]
-
+  
   return(dt$Err)
 }
