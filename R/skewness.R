e2c34d3306824356200b5e8e38db4bc0b656de02
Ben Gorman <bgorman519@gmail.com>
2016-11-23 16:53:49 -0600

    Added docs for skewness()

--- R/skewness.R
+++ R/skewness.R
@@ -1,7 +1,20 @@
+#' @title
 #' Skewness
 #'
+#' @description
+#' Calculates the skewness of each field in a data.table
+#'
+#' @details
+#' Counts the frequency of each value in each column, then displays the results in descending order
+#' 
+#' @param dt A data.table
+#' 
 #' @export
 #' @import data.table
+#'
+#' @examples
+#' library(data.table)
+#' skewness(alien.train)
 
 skewness <- function(dt){
   # Display the skewness of each field
@@ -14,7 +27,6 @@ skewness <- function(dt){
   #--------------------------------------------------
 
   skewnessList <- list()
-
   for(colname in colnames(dt)){
     skewnessList[[colname]] <- dt[, list(Count=.N, Pcnt=.N/nrow(dt)), by=colname][order(Count, decreasing=TRUE)]
   }
