d838fe143947bdf54f5c4aca71c7fbb91c92a5fa
Ben Gorman <bgorman519@gmail.com>
2016-11-23 16:34:54 -0600

    Added some documentation for explore_dataset()

--- R/explore_dataset.R
+++ R/explore_dataset.R
@@ -1,7 +1,25 @@
-#' Explore a dataset
+#' @title
+#' Explore Dataset
+#'
+#' @description
+#' (Expirimental) Automated Exploratory Data Analysis
+#'
+#' @details
+#' Expirimental. Evaluates and summarizes the data in every column of a data.table. 
+#' Can identify columns with hierarchical structure and columns with perfectly correlated 
+#' values.
+#' 
+#' @param dt1 dataset to analyze
+#' @param dt2 (optional) second dataset to analyze, with the same columns as dt1
+#' @param targetCol Name of the column you're trying to model/predict
+#' @param verbose Should the exploratory process steps be displayed?
 #'
 #' @export
 #' @import data.table
+#'
+#' @examples
+#' library(data.table)
+#' explore_dataset(alien.train)
 
 explore_dataset <- function(dt1, dt2=NULL, targetCol=NULL, verbose=FALSE){
   # Analyze the given dataset(s)
@@ -25,7 +43,7 @@ explore_dataset <- function(dt1, dt2=NULL, targetCol=NULL, verbose=FALSE){
   # if (i,j) = 0 and (j, i) > 0 then field i is (likely) a child of field j (i.e. a hierarchy structure is present)
 
   varpairs <- gini_impurities(dt1, verbose=verbose)
-  giniMatrix <- dcast(varpairs, Var1 ~ Var2, value.var="GI")
+  giniMatrix <- dcast(varpairs, Var1 ~ Var2, value.var="GiniImpurity")
 
   #--------------------------------------------------
   # Field skewness
