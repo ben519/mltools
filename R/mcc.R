5091582aa379eb036e9e6df9e3c4be97be70bdde
fanli-gcb <fanli.gcb@gmail.com>
2017-12-15 14:49:18 -0800

    Added support for multiclass MCC

--- R/mcc.R
+++ R/mcc.R
@@ -2,70 +2,51 @@
 #' Matthews correlation coefficient
 #'
 #' @description
-#' Calculate Matthews correlation coefficient
+#' Calculate Matthews correlation coefficient, with support for the multi-class definition:
+#' \deqn{MCC = \frac{cov(X,Y)}{\sqrt{cov(X,X)*cov(Y,Y)}}}{MCC = cov(X,Y) / sqrt(cov(X,X)*cov(Y,Y))}
 #'
 #' @details
 #' Calculate Matthews correlation coefficient. Either
 #' 
 #' \itemize{
-#'  \item{\code{preds} and \code{actuals} can be given, leaving \code{TP}, \code{FP}, \code{TN}, and \code{FN} NULL} or
-#'  \item{\code{TP}, \code{FP}, \code{TN}, and \code{FN} can be given, leaving \code{preds} and \code{actuals} NULL}
+#'  \item{\code{preds} and \code{actuals} can be given, in which case \code{x} and \code{y} will be ignored} or
+#'  \item{\code{x} and \code{y} can be given, leaving \code{preds} and \code{actuals} NULL}
 #' }
 #' 
-#' @param preds A vector of prediction values in {1, 0}, or {TRUE, FALSE}
-#' @param actuals A vector of actuals values in {1, 0}, or {TRUE, FALSE}
-#' @param TP Count of true positives (correctly predicted 1/TRUE)
-#' @param FP Count of false positives (predicted 1/TRUE, but actually 0/FALSE)
-#' @param TN Count of true negatives (correctly predicted 0/FALSE)
-#' @param FN Count of false negatives (predicted 0/FALSE, but actually 1/TRUE)
+#' @param preds A vector of predicted classes in \{0, 1, ...\}
+#' @param actuals A vector of actual classes in \{0, 1, ...\}
+#' @param x Indicator matrix of predicted classes (rows are samples, columns are classes). \eqn{x_{ij} = 1} if sample i is predicted as class j, 0 otherwise.
+#' @param y Indicator matrix of actual classes (rows are samples, columns are classes). \eqn{y_{ij} = 1} if sample i is labelled as class j, 0 otherwise.
 #'
 #' @references
-#' \url{https://en.wikipedia.org/wiki/Matthews_correlation_coefficient}
-#'
-#' @examples
-#' preds <- c(1,1,1,0,1,1,0,0)
-#' actuals <- c(1,1,1,1,0,0,0,0)
-#' mcc(preds, actuals)
-#' mcc(actuals, actuals)
-#' mcc(TP=3, FP=2, TN=2, FN=1)
+#' \url{https://doi.org/10.1371/journal.pone.0041882}
+#' \url{https://doi.org/10.1016/j.compbiolchem.2004.09.006}
 #' 
-#' @export
-#' @import data.table
 
-mcc <- function(preds=NULL, actuals=NULL, TP=NULL, FP=NULL, TN=NULL, FN=NULL){
-  # Matthews correlation coefficient
-  # Either preds and actuals should be given or TP, FP, TN, and FN should be given
-  # preds and actuals should be vectors with values in {0, 1} or {TRUE, FALSE}
-  # TP, FP, TN, FN should be values representing the count of True Positives, False Positives, True Negatives, False Negatives
-  
-  valid_input <- FALSE
-  if(is.null(preds) & is.null(actuals) & (!is.null(TP) & !is.null(FP) & !is.null(TN) & !is.null(FN))) valid_input <- TRUE
-  if(!is.null(preds) & !is.null(actuals) & (is.null(TP) & is.null(FP) & is.null(TN) & is.null(FN))) valid_input <- TRUE
-  if(!valid_input) stop("Either preds and actuals should be provided or TP, FP, TN, and FN should be provided")
-  
-  # If preds/actuals provided, measure TP, FP, TN, FN
-  if(!is.null(preds)){
-    
-    # Check input values
-    if(sum(!preds %in% c(0, 1)) + sum(!actuals %in% c(0, 1)) > 0) stop("preds and actuals must be vectors of {1, 0} or {TRUE, FALSE}")
-    
-    TP <- sum(actuals[preds==1] == 1)
-    FP <- sum(actuals[preds==1] == 0)
-    TN <- sum(actuals[preds==0] == 0)
-    FN <- sum(actuals[preds==0] == 1)
-  }
-  
-  # Convert types to double for better precision
-  TP <- as.double(TP)
-  FP <- as.double(FP)
-  TN <- as.double(TN)
-  FN <- as.double(FN)
-  
-  # Calculate MCC
-  numerator <- (TP * TN - FP * FN)
-  denominator <- sqrt((TP + FP)*(TP + FN)*(TN + FP)*(TN + FN))
-  if(denominator == 0) denominator <- 1
-  mcc <- numerator/denominator
-  
-  return(mcc)
+mcc <- function(preds=NULL, actuals=NULL, x=NULL, y=NULL) {
+	# if preds and actuals are provided, x and y will be ignored
+	if (!is.null(preds)) {
+		nclasses <- length(union(preds, actuals))
+		x <- matrix(0, nrow=length(preds), ncol=nclasses)
+		y <- matrix(0, nrow=length(actuals), ncol=nclasses)
+		x[cbind(1:nrow(x), preds+1)] <- 1
+		y[cbind(1:nrow(y), actuals+1)] <- 1
+	}
+	if (!all(dim(x) == dim(y))) {
+		stop("X and Y must have the same dimensions")
+	}
+	
+	cov_biased <- function(x, y) {
+		sum(sapply(1:ncol(x), function(k) {
+			cov(x[,k], y[,k]) # unbiased estimate with (n-1) denominator as opposed to (n), but cancels out anyways so identical result
+		}))
+	}
+	numerator <- cov_biased(x,y)
+	denominator <- sqrt(cov_biased(x,x) * cov_biased(y,y))
+	retval <- numerator / denominator	
+	return(retval)
 }
+
+
+
+
